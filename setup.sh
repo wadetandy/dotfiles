ce6fba94213ca62eeb2e29884b6b1856e5354c11
Wade Tandy <wade@wadetandy.com>
2012-01-12 16:08:34 -0500

    update setup script to move .vim directory into place and pull submodules from github

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CUR_DIR=`pwd`
 HOME=`echo ~/`
 DOTFILES=$(cd $(dirname "$0"); pwd)
 
@@ -7,3 +8,14 @@ ln -s $DOTFILES/vimrc $HOME/.vimrc
 ln -s $DOTFILES/bashrc $HOME/.bashrc
 ln -s $DOTFILES/inputrc $HOME/.inputrc
 
+mv $HOME/.vim $HOME/.bak.vim
+ln -s $DOTFILES/.vim $HOME/.vim
+
+mkdir -p $DOTFILES/.vim/tmp/backup
+mkdir -p $DOTFILES/.vim/tmp/swap
+mkdir -p $DOTFILES/.vim/tmp/undo
+
+cd $DOTFILES/.vim
+git submodule update --init
+
+cd $CUR_DIR
