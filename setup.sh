66c65321c84ecf72a289728d04114718824969a6
Wade Tandy <wade@wadetandy.com>
2013-01-07 23:22:53 -0500

    change to copy garvin's way of setting up vim

--- setup.sh
+++ setup.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
+# Downloads dotfiles repository to current directory, then links vim dot-files
+# into current user's home directory.
+#
+# Requires git.
+#
+# Easy run:
+#
+#   curl https://raw.github.com/coderifous/dotfiles/master/get.vimfiles.sh | sh
+
+# Put your github username on the next line
+REPO_OWNER="wadetandy"
+# REPO_HOST will generally be "github.com", but may be changed to something else
+# if you're using a different git hosting service or if you have configured
+# an alias in ~/.ssh/config, e.g. because you're using multiple identities.
+REPO_HOST="github.com"
+# The next line should contain the name of the repository.
+REPO_NAME="dotfiles"
+GIT_REPO_URL="git@$REPO_HOST:$REPO_OWNER/$REPO_NAME.git"
+
+echo -e "\033[32mDownloading repository."
+echo -e "\033[0m"
+
+git clone $GIT_REPO_URL
+
+echo
+echo -e "\033[32mUpdating submodules."
+echo -e "\033[0m"
+
+pushd $REPO_NAME
+git submodule update --init
+
+echo
+echo -e "\033[32mCreating dotfile links in home dir."
+echo -e "\033[0m"
+
+VIMHOME=`pwd`"/vim"
+
+ln -s $VIMHOME      ~/.vim
+ln -s ~/.vim/vimrc  ~/.vimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
+
+echo
+echo -e "\033[32mVim dotfiles installed!"
+echo -e "\033[0m"
 
 CUR_DIR=`pwd`
 HOME=`echo ~/`
 DOTFILES=$(cd $(dirname "$0"); pwd)
 
-ln -s $DOTFILES/vimrc $HOME/.vimrc
-ln -s $DOTFILES/bashrc $HOME/.bashrc
-ln -s $DOTFILES/inputrc $HOME/.inputrc
-
-mv $HOME/.vim $HOME/.bak.vim
-ln -s $DOTFILES/.vim $HOME/.vim
-
-mkdir -p $DOTFILES/.vim/tmp/backup
-mkdir -p $DOTFILES/.vim/tmp/swap
-mkdir -p $DOTFILES/.vim/tmp/undo
-
-cd $DOTFILES/.vim
-git submodule update --init
-
-cd $CUR_DIR
+popd
